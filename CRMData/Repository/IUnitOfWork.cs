1c705cc45911b178c03756cc961413874cb1936a
oleksandr.dubyna <csharp.agency@hotmail.com>
2018-05-09 21:55:41 +0300

    add UnitOfWork

---
+++ CRMData/Repository/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using CRMData.Contexts;
+using CRMData.Entities;
+using System;
+
+namespace CRMData.Repository
+{
+	public interface IUnitOfWork : IDisposable
+	{
+		BaseContext Context { get; }
+		EFGenericRepository<Lead> LeadsRepository { get; }
+	}
+}
---
+++ CRMData/Repository/UnitOfWork.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using CRMData.Contexts;
+using CRMData.Entities;
+
+namespace CRMData.Repository
+{
+	public class UnitOfWork : IUnitOfWork
+	{
+		private BaseContext _dbContext;
+
+		private EFGenericRepository<Lead> _leadsRepository;
+
+		public UnitOfWork()
+		{
+			_dbContext = new BaseContext();
+		}
+
+		public UnitOfWork(BaseContext context)
+		{
+			_dbContext = context;
+		}
+
+		public BaseContext Context => _dbContext;
+
+		public EFGenericRepository<Lead> LeadsRepository
+		{
+			get
+			{
+				if (_leadsRepository == null)
+				{
+					_leadsRepository = new EFGenericRepository<Lead>(_dbContext);
+				}
+
+				return _leadsRepository;
+			}
+		}
+
+		private bool disposed = false;
+
+		public virtual void Dispose(bool disposing)
+		{
+			if (!this.disposed)
+			{
+				if (disposing)
+				{
+					_dbContext.Dispose();
+				}
+
+				this.disposed = true;
+			}
+		}
+
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+	}
+}
