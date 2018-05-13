b0f23d82bf820a753c4c14c8d514b4ef7b89194c
oleksandr.dubyna <csharp.agency@hotmail.com>
2018-05-13 22:02:37 +0300

    Implement Run Seed after migrations

--- CRMData/Contexts/CRMDBInitializer.cs
+++ CRMData/Contexts/CRMDBInitializer.cs
@@ -3,24 +3,29 @@ using System.Data.Entity;
 
 namespace CRM.DAL.Contexts
 {
-	public class CrmdbInitializer : CreateDatabaseIfNotExists<BaseContext>
-    {
-        protected override void Seed(BaseContext context)
-        {
-            context.DPhoneTypes.Add(new DPhoneType() { TypeName = "HomePhone" });
-            context.DPhoneTypes.Add(new DPhoneType() { TypeName = "WorkPhone" });
-            context.DPhoneTypes.Add(new DPhoneType() { TypeName = "MobilePhone" });
-            context.DPhoneTypes.Add(new DPhoneType() { TypeName = "EmergencyContactPhone" });
-            context.DPhoneTypes.Add(new DPhoneType() { TypeName = "Fax" });
+	public class CrmDbInitializer : CreateDatabaseIfNotExists<BaseContext>
+	{
+		protected override void Seed(BaseContext context)
+		{
+			context.DPhoneTypes.Add(new DPhoneType() { TypeName = "HomePhone" });
+			context.DPhoneTypes.Add(new DPhoneType() { TypeName = "WorkPhone" });
+			context.DPhoneTypes.Add(new DPhoneType() { TypeName = "MobilePhone" });
+			context.DPhoneTypes.Add(new DPhoneType() { TypeName = "EmergencyContactPhone" });
+			context.DPhoneTypes.Add(new DPhoneType() { TypeName = "Fax" });
 
-            context.DAddressTypes.Add(new DAddressType() { TypeName = "BillingAddress" });
-            context.DAddressTypes.Add(new DAddressType() { TypeName = "ContactAddress" });
-            context.DAddressTypes.Add(new DAddressType() { TypeName = "EmergencyContactAddress" });
+			context.DAddressTypes.Add(new DAddressType() { TypeName = "BillingAddress" });
+			context.DAddressTypes.Add(new DAddressType() { TypeName = "ContactAddress" });
+			context.DAddressTypes.Add(new DAddressType() { TypeName = "EmergencyContactAddress" });
 
-            context.DUserTypes.Add(new DUserType() { TypeName = "AdminTeamMember" });
-            context.DUserTypes.Add(new DUserType() { TypeName = "Customer" });
+			context.DUserTypes.Add(new DUserType() { TypeName = "AdminTeamMember" });
+			context.DUserTypes.Add(new DUserType() { TypeName = "Customer" });
 
-            base.Seed(context);
-        }
-    }
+			base.Seed(context);
+		}
+
+		public static void InitDataInDB(BaseContext db)
+		{
+			new CrmDbInitializer().Seed(db);
+		}
+	}
 }
