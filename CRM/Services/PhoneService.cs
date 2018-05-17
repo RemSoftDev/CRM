4ca68979769f693241a40302dc40bb32edd3e1c2
csharp2 <zinchenko.sl.ua@gmail.com>
2018-05-17 22:59:22 +0300

    commit before merge

---
+++ CRM/Services/PhoneService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CRM.Services
+{
+    public class PhoneService
+    {
+        public PhoneService()
+        {
+
+        }
+
+        public string GetRedirectUrlByPhoneNum(string phoneNum)
+        {
+            return null
+        }
+    }
+}
